a7d91904237bfbbf559591cbe0b83eab5a81082d
root <root@mgr.ispsystem.net>
2015-06-22 10:47:11 +0800

    apply autoformat

--- checker.cpp
+++ checker.cpp
@@ -7,48 +7,50 @@
 
 MODULE("seodns_checker");
 
-int ISP_MAIN(int argc, _TCHAR *argv[])
+int ISP_MAIN(int argc, _TCHAR* argv[])
 {
-	mgr_log::Init("dnschecker");
-        mgr_client::Local con("dnsmgr", "seodns_checker");
+    mgr_log::Init("dnschecker");
+    mgr_client::Local con("dnsmgr", "seodns_checker");
 
-	auto db = mgr_db::Connect(con);
-	ForEachQuery(db, "SELECT d.name, u.name, u.dnsns FROM domain d LEFT JOIN user u on d.user=u.id WHERE d.seodnsparked='on'", domain_list) {
-		LogExt("check domain %s", domain_list->AsString(0).c_str());
-		auto whois = mgr_proc::Execute("whois "+domain_list->AsString(0), mgr_proc::Execute::efOut);
-		string out = str::Lower(whois.Str());
-		Debug("result=%d out %s\n", whois.Result(), out.c_str());
+    auto db = mgr_db::Connect(con);
+    ForEachQuery(db, "SELECT d.name, u.name, u.dnsns FROM domain d LEFT JOIN user u on d.user=u.id WHERE d.seodnsparked='on'", domain_list)
+    {
+        LogExt("check domain %s", domain_list->AsString(0).c_str());
+        auto whois = mgr_proc::Execute("whois " + domain_list->AsString(0), mgr_proc::Execute::efOut);
+        string out = str::Lower(whois.Str());
+        Debug("result=%d out %s\n", whois.Result(), out.c_str());
 
-		if (whois.Result() && out.find("no whois server") != string::npos) {
-			// unknown tld
-			con.Query("func=domain.delete&elid="+domain_list->AsString(0)+"&su="+domain_list->AsString(1));
-		} else if (whois.Result() == 0) {
-			// looking for nameservers
-			StringSet WhoisNameServers, LocalNameServers;
-			while (!out.empty()) {
-				string line = str::GetWord(out, "\n");
-				if (line.find("name server:") != string::npos 
-				|| line.find("nserver:") != string::npos) {
-					WhoisNameServers.insert(str::RGetWord(line));
-				}
-			}
+        if (whois.Result() && out.find("no whois server") != string::npos) {
+            // unknown tld
+            con.Query("func=domain.delete&elid=" + domain_list->AsString(0) + "&su=" + domain_list->AsString(1));
+        } else if (whois.Result() == 0) {
+            // looking for nameservers
+            StringSet WhoisNameServers, LocalNameServers;
+            while (!out.empty()) {
+                string line = str::GetWord(out, "\n");
+                if (line.find("name server:") != string::npos
+                    || line.find("nserver:") != string::npos) {
+                    WhoisNameServers.insert(str::RGetWord(line));
+                }
+            }
 
-			str::Split(domain_list->AsString(2), " ", LocalNameServers);
+            str::Split(domain_list->AsString(2), " ", LocalNameServers);
 
-			// compare name servers
-			bool found = false;
-			ForEachI(WhoisNameServers, n) {
-				LogExt("Check whois nameserver %s", n->c_str());
-				if (LocalNameServers.find(*n) != LocalNameServers.end()) {
-					found = true;
-					break;
-				}
-			}
-			if (!found)
-				con.Query("func=domain.delete&elid="+domain_list->AsString(0)+"&su="+domain_list->AsString(1));
-		}
-		sleep(1); // for whois block system
-	}
+            // compare name servers
+            bool found = false;
+            ForEachI(WhoisNameServers, n)
+            {
+                LogExt("Check whois nameserver %s", n->c_str());
+                if (LocalNameServers.find(*n) != LocalNameServers.end()) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+                con.Query("func=domain.delete&elid=" + domain_list->AsString(0) + "&su=" + domain_list->AsString(1));
+        }
+        sleep(1); // for whois block system
+    }
 
-	return 0;
+    return 0;
 }
--- seodns.cpp
+++ seodns.cpp
@@ -2,8 +2,8 @@
 #include <mgr/mgrlog.h>
 #include <mgr/mgrclient.h>
 #include <mgr/mgrproc.h>
-#include <api/action.h> 
-#include <mgr/mgrdb_struct.h> 
+#include <api/action.h>
+#include <mgr/mgrdb_struct.h>
 #include <api/stddb.h>
 #include <api/autotask.h>
 #include <dnsmgr/db.h>
@@ -14,106 +14,127 @@ namespace {
 DEFINE_FAIL("seodns");
 using namespace isp_api;
 
-mgr_db::JobCache *db;
+mgr_db::JobCache* db;
 
 class EventUserDelete : public Event {
 public:
-	EventUserDelete(): Event("user.delete.one", "seodns") { }
+    EventUserDelete()
+        : Event("user.delete.one", "seodns")
+    {
+    }
 
-	void BeforeExecute(Session& ses) const {
-		auto user_table = db->Get<UserTable>();
-		if (user_table->FindByName(ses.Param("elid")) && !user_table->Parent.IsNull()) {
-			auto reseller_table = db->Get<UserTable>();
-			if (reseller_table->Find(user_table->Parent) && (int)reseller_table->NameSpace == user_table->NameSpace) {
-				ForEachQuery(db, "SELECT name FROM domain WHERE user="+user_table->Id, domain_list) {
-					InternalCall("domain.delete", "elid="+domain_list->AsString(0));
-				}
-			}
-		}
-	}
+    void BeforeExecute(Session& ses) const
+    {
+        auto user_table = db->Get<UserTable>();
+        if (user_table->FindByName(ses.Param("elid")) && !user_table->Parent.IsNull()) {
+            auto reseller_table = db->Get<UserTable>();
+            if (reseller_table->Find(user_table->Parent) && (int)reseller_table->NameSpace == user_table->NameSpace) {
+                ForEachQuery(db, "SELECT name FROM domain WHERE user=" + user_table->Id, domain_list)
+                {
+                    InternalCall("domain.delete", "elid=" + domain_list->AsString(0));
+                }
+            }
+        }
+    }
 };
 
 class EventDomainCreate : public Event {
 public:
-	EventDomainCreate(): Event("domain.edit", "seodns") { }
+    EventDomainCreate()
+        : Event("domain.edit", "seodns")
+    {
+    }
 
-	void BeforeExecute(Session& ses) const {
-		if (!ses.Param("sok").empty() && ses.Param("elid").empty()) {
-			auto domain_table = db->Get<DomainTable>();
-			if (domain_table->FindByName(ses.Param("name")) && domain_table->FieldByName("seodnsparked")->AsString() == "on") {
-				InternalCall("domain.delete", "elid="+ses.Param("name"));
-			}
-		}
-	}
+    void BeforeExecute(Session& ses) const
+    {
+        if (!ses.Param("sok").empty() && ses.Param("elid").empty()) {
+            auto domain_table = db->Get<DomainTable>();
+            if (domain_table->FindByName(ses.Param("name")) && domain_table->FieldByName("seodnsparked")->AsString() == "on") {
+                InternalCall("domain.delete", "elid=" + ses.Param("name"));
+            }
+        }
+    }
 };
 
 class EventDomainDelete : public Event {
 public:
-	EventDomainDelete(): Event("domain.delete.one", "seodns") { }
+    EventDomainDelete()
+        : Event("domain.delete.one", "seodns")
+    {
+    }
 
-	void BeforeExecute(Session& ses) const {
-		auto domain_table = db->Get<DomainTable>();
-		auto user_table = db->Get<UserTable>();
+    void BeforeExecute(Session& ses) const
+    {
+        auto domain_table = db->Get<DomainTable>();
+        auto user_table = db->Get<UserTable>();
 
-		ses.DelParam("new_domain_owner");
-		if (domain_table->FindByName(ses.Param("elid"))
-		  && user_table->Find(domain_table->User)
-		  && !user_table->Parent.IsNull()) {
-			auto reseller_table = db->Get<UserTable>();
-			if (reseller_table->Find(user_table->Parent) && (int)reseller_table->NameSpace == user_table->NameSpace)
-				ses.SetParam("new_domain_owner", user_table->Parent);
-		}
-	}
+        ses.DelParam("new_domain_owner");
+        if (domain_table->FindByName(ses.Param("elid"))
+            && user_table->Find(domain_table->User)
+            && !user_table->Parent.IsNull()) {
+            auto reseller_table = db->Get<UserTable>();
+            if (reseller_table->Find(user_table->Parent) && (int)reseller_table->NameSpace == user_table->NameSpace)
+                ses.SetParam("new_domain_owner", user_table->Parent);
+        }
+    }
 
-	void AfterExecute(Session& ses) const {
-		string domain = ses.Param("elid");
-		string owner = ses.Param("new_domain_owner");
-		Debug("delete domain '%s' reseller=%s", domain.c_str(), owner.c_str());
+    void AfterExecute(Session& ses) const
+    {
+        string domain = ses.Param("elid");
+        string owner = ses.Param("new_domain_owner");
+        Debug("delete domain '%s' reseller=%s", domain.c_str(), owner.c_str());
 
-		if (!owner.empty()) {
-			try {
-				auto user_table = db->Get<UserTable>();
-				user_table->Assert(owner);
-				InternalCall("domain.edit", "su="+user_table->Name+"&sok=ok&name="+domain+"&dtype=master&ip="+user_table->FieldByName("seodnsip")->AsString());
+        if (!owner.empty()) {
+            try {
+                auto user_table = db->Get<UserTable>();
+                user_table->Assert(owner);
+                InternalCall("domain.edit", "su=" + user_table->Name + "&sok=ok&name=" + domain + "&dtype=master&ip=" + user_table->FieldByName("seodnsip")->AsString());
 
-				auto domain_table = db->Get<DomainTable>();
-				domain_table->AssertByName(domain);
-				domain_table->FieldByName("seodnsparked")->Set("on");
-				domain_table->Post();
-			} catch (...) { }
-		}
-	}
+                auto domain_table = db->Get<DomainTable>();
+                domain_table->AssertByName(domain);
+                domain_table->FieldByName("seodnsparked")->Set("on");
+                domain_table->Post();
+            }
+            catch (...) {
+            }
+        }
+    }
 };
 
 class EventDnsParam : public Event {
 public:
-	EventDnsParam(): Event("dnsparam", "seodns") { }
+    EventDnsParam()
+        : Event("dnsparam", "seodns")
+    {
+    }
 
-	void AfterExecute(Session& ses) const {
-		auto user_table = db->Get<UserTable>();
-		user_table->Assert(ses.auth.ext("uid"));
-		
-		if (ses.Param("sok").empty()) {
-			ses.NewNode("seodnsip", user_table->FieldByName("seodnsip")->AsString());
-		} else {
-			user_table->FieldByName("seodnsip")->Set(ses.Param("seodnsip"));
-			user_table->Post();
-		}
-	}
+    void AfterExecute(Session& ses) const
+    {
+        auto user_table = db->Get<UserTable>();
+        user_table->Assert(ses.auth.ext("uid"));
+
+        if (ses.Param("sok").empty()) {
+            ses.NewNode("seodnsip", user_table->FieldByName("seodnsip")->AsString());
+        } else {
+            user_table->FieldByName("seodnsip")->Set(ses.Param("seodnsip"));
+            user_table->Post();
+        }
+    }
 };
 
-MODULE_INIT(seodns, "") {
-	db = GetDb();
+MODULE_INIT(seodns, "")
+{
+    db = GetDb();
 
-	new EventDnsParam();
+    new EventDnsParam();
 
-	new EventUserDelete();
+    new EventUserDelete();
 
-	new EventDomainCreate();
-	new EventDomainDelete();
+    new EventDomainCreate();
+    new EventDomainDelete();
 
-	string cmd = mgr_file::ConcatPath(mgr_file::GetCurrentDir(), "sbin/seodns_checker");
-	isp_api::task::Schedule(cmd, "0 2 * * *", "check redelegated domains");
+    string cmd = mgr_file::ConcatPath(mgr_file::GetCurrentDir(), "sbin/seodns_checker");
+    isp_api::task::Schedule(cmd, "0 2 * * *", "check redelegated domains");
 }
 
 } // end of private namespace
