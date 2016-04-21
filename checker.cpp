e7bab7b8e7a1a3800c5191549c40cb8fd5e35941
root <root@msk-dns2.ispsystem.net>
2016-04-21 06:33:48 +0300

    delete domain if whois not respond long time

--- checker.cpp
+++ checker.cpp
@@ -15,6 +15,7 @@ int ISP_MAIN(int argc, _TCHAR* argv[])
     auto db = mgr_db::Connect(con);
     ForEachQuery(db, "SELECT d.name, u.name, u.dnsns FROM domain d LEFT JOIN user u on d.user=u.id WHERE d.seodnsparked='on'", domain_list)
     {
+        bool found = false;
         LogExt("check domain %s", domain_list->AsString(0).c_str());
         mgr_file::Tmp tmp("tmp/out");
         mgr_proc::Execute whois("whois " + domain_list->AsString(0) + " > " + tmp.Str());
@@ -22,36 +23,32 @@ int ISP_MAIN(int argc, _TCHAR* argv[])
         // sometime whois hungs for long time, so we kill it after 10 seconds
         for (int i = 100; i && whois.IsAlive(); --i)
             mgr_proc::Sleep(100);
+
         if (whois.IsAlive()) {
             whois.Terminate();
-            continue;
-        }
-        //auto whois = mgr_proc::Execute("whois " + domain_list->AsString(0), mgr_proc::Execute::efOutErr);
-        //string out = str::Lower(whois.Str());
-        string out = str::Lower(mgr_file::Read(tmp));
-        Debug("result=%d out '%s'\n", whois.Result(), out.c_str());
+        } else {
+        	string out = str::Lower(mgr_file::Read(tmp));
+        	Debug("result=%d out '%s'\n\n", whois.Result(), out.c_str());
 
-        if (whois.Result() == 0) {
-            // looking for nameservers
-            StringSet LocalNameServers;
-            str::Split(domain_list->AsString(2), " ", LocalNameServers);
-            bool found = false;
-            ForEachI(LocalNameServers, srv)
-            {
-                string n = *srv;
-                if (n[n.size() - 1] == '.') {
-                    n.erase(n.size() - 1);
-                    if (out.find(n) != string::npos) {
-                        found = true;
-                        break;
-                    }
-                }
-            }
-
-            if (!found)
-                con.Query("func=domain.delete&elid=" + domain_list->AsString(0) + "&su=" + domain_list->AsString(1));
-        }
-        sleep(1); // for whois block system
+        	if (whois.Result() == 0) {
+            		// looking for nameservers
+            		StringSet LocalNameServers;
+            		str::Split(domain_list->AsString(2), " ", LocalNameServers);
+            		ForEachI(LocalNameServers, srv) {
+                		string n = *srv;
+                		if (n[n.size() - 1] == '.') {
+                    			n.erase(n.size() - 1);
+                    			if (out.find(n) != string::npos) {
+                        			found = true;
+                        			break;
+                    			}
+                		}
+            		}
+        		sleep(1); // for whois block system
+        	}
+	}
+        if (!found) 
+        	con.Query("func=domain.delete&elid=" + domain_list->AsString(0) + "&su=" + domain_list->AsString(1));
     }
 
     return 0;
